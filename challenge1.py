eb92442e817fe1c41cd8bbedd1cb864d6e907427
Ayman Ahmed <absi.ayman@gmail.com>
2018-10-20 09:48:16 +0300

    Print Udacian

---
+++ challenge1.py
@@ -0,0 +1,11 @@
+def smash(words):
+    separtor = ' '
+    return separtor.join(words)
+
+def run():
+    words = ['hello', 'world', 'this', 'is', 'greate']
+    words.reverse()
+    print(words)
+
+
+run()
