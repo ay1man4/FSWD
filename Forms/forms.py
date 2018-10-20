3b70eaf033ad45c6cb78c5aab1eaa12dc632583d
Ayman Ahmed <absi.ayman@gmail.com>
2018-10-20 18:45:31 +0300

    Solve Exercise

--- Forms/forms.py
+++ Forms/forms.py
@@ -35,33 +35,18 @@ class MessageHandler(BaseHTTPRequestHandler):
         # 2. Read the correct amount of data from the request.
         data = self.rfile.read(length).decode()
 
-         # 3. Extract the "message" field from the request data.
-        ud = Udacian(parse_qs(data)["name"][0], parse_qs(data)["city"][0], parse_qs(data)["enrollment"][0]
-        , parse_qs(data)["nanodegree"][0], parse_qs(data)["status"][0])
-        # memory.append(parse_qs(data)["name"][0])
-        # memory.append(parse_qs(data)["city"][0])
-        # memory.append(parse_qs(data)["enrollment"][0])
-        # memory.append(parse_qs(data)["nanodegree"][0])
-        # memory.append(parse_qs(data)["status"][0])
-
+         # 3. Extract all parameters to dictionay d from the request data.
+        d = parse_qs(data)
+        ud = Udacian(d['name'][0], d['city'][0], d['enrollment'][0], d['nanodegree'][0], d['status'][0])
+        
+        # Add new udacian to memory
         memory.append(ud)
 
-        # response = '''
-        # <h1>{0}</h1>
-        # <p>{1}</p>
-        # <p>{2}</p>
-        # <p>{3}</p>
-        # <p>{4}</p>
-        # '''.format(u)
-        response = memory[0]
-
-        # Send the "message" field back as the response.
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html; charset=utf-8')
+        # redirect user to home page
+        self.send_response(303)
+        self.send_header('Location', '/')
         self.end_headers()
-
-        self.wfile.write(response.encode())
-
+        
     def do_GET(self):
         # First, send a 200 OK response.
         self.send_response(200)
@@ -70,8 +55,14 @@ class MessageHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
 
+        # Join all udacians stored in memory to single string (one udacian per line)
+        s = "\n".join(str(u) for u in memory)
+
+        # replace {} in form with udacians string
+        form_with_history = form.format(s)
+        
         # Now, write the response body.
-        self.wfile.write(form.encode())
+        self.wfile.write(form_with_history.encode())
 
 if __name__ == '__main__':
     server_address = ('', 8000)
--- Forms/udacian.py
+++ Forms/udacian.py
@@ -6,6 +6,6 @@ class Udacian:
         self.nanodegree = nanodegree
         self.status = status
 
-
+    # by override __str__ we can use method str(udacian) to print Udacian object
     def __str__(self):
-        return self.name + "; " + self.city + ", " + self.enrollment + ", " + self.nanodegree + ", " + self.status
+        return self.name + ": " + self.city + ", " + self.enrollment + ", " + self.nanodegree + ", " + self.status
